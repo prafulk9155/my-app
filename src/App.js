b52dc56eefe0a59f1ade951b1d9d1fac5a3965b4
Praful Kumar <prafulk9155@gmail.com>
2023-01-31 12:55:20 +0530

    frontend added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import close_eye from './images/close_eye.png';
+import  from './images/close_eye.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={close_eye} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h3>Hey! This is Jarvis World !!!!</h3> 
+        
+        
       </header>
     </div>
   );
